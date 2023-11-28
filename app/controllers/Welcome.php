0163cb15b973c2a3d31024902a9580d34905a7e3
Johnloydgutierrez <gutierrezjl750@gmail.com>
2023-11-28 08:22:04 +0800

    insert

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -2,6 +2,11 @@
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class Welcome extends Controller {
+	public function __construct() {
+		parent::__construct();
+		$this->call->model('main_model');
+	}
+
 	public function index() {
 		$this->call->view('Nhome');
 	}
@@ -17,20 +22,43 @@ class Welcome extends Controller {
 	public function signin() {
 		$this->call->view('signin');
 	}
+	public function showassignment() {
+		$this->call->view('assignment');
+	}
+	public function index1() {
+		$this->call->view('index1');
+	}
 	public function AddAssignment() {
-		$this->call->view('AddAssignment');
+		if ($this->form_validation->submitted()) {
+			$this->form_validation
+
+			->name('employee')
+
+			->name('description')
+
+			->name('location')
+
+			->name('contact')
+
+			->name('customer')
+			->required();
+			
+			if ($this->form_validation->run())
+			{
+				$this->main_model->insert($this->io->post('employee'), $this->io->post('description'), $this-> io->post('location'), $this->io->post('contact'), $this->io->post('customer'));
+				redirect('/AddAssignment');
+				exit();
+			}
+			else{
+				$this->call->view('AddAssignment');
+			}
+
+				}	
+			$data=$this->main_model->table();	
+			$this->call->view('AddAssignment', $data);
 	}
 
-	public function insert() {
-		$bind = array(
-			'employee' => $employee,
-			'description' => $description,
-			'location' => $location,
-			'contact' => $contact,
-			'customer' => $customer,
-			);
-		
-		$this->db->table('addasign')->insert($bind);
-	}
+	
+
 }
 ?>
